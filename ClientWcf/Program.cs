f13ca0f727e4b05bcfc5281480a14efc5f5bab56
Ajinkya Bhagwat <abhagwat@tavisca.com>
2016-02-15 21:36:58 +0530

    set up basic project

---
+++ ClientWcf/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientWcf
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new Service1Client();
+            var a = client.GetData(43);
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleWcf/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using HelloWcf;
+
+namespace ConsoleWcf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(Service1));
+            try
+            {
+                host.Open();
+                foreach (var channelDispatcher in host.ChannelDispatchers)
+                {
+                    if (channelDispatcher.Listener != null) Console.WriteLine(channelDispatcher.Listener.Uri.Port);
+                }
+                Console.WriteLine("started");
+                Console.ReadLine();
+                host.Close();
+            }
+            catch (Exception exception)
+            {
+                host.Close();
+            }
+        }
+    }
+}
